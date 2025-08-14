575262d172f8fcae4e5e1d7402bb8d11197cdeeb
Mateus Valente <mateus.valente@outlook.com.br>
2025-08-13 22:27:12 -0300

    Meu primeiro commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Exibe Texto
+print('Jornada de Dados')
+
+# Variáveis
+nome = 'Mateus Valente'
+idade = 31
+
+# Exibe variáveis
+
+print('Meu nome é: ' + nome)
+print('Eu tenho: ' + idade + 'anos.')
