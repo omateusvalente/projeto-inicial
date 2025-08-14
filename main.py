8a8d8bc325a8b4a2c391981b97421133a60309be
Mateus Valente <mateus.valente@outlook.com.br>
2025-08-13 22:40:22 -0300

    main.py att

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ print('Jornada de Dados')
 
 # Variáveis
 nome = 'Mateus Valente'
-idade = 31
+idade = '31'
 
 # Exibe variáveis
 
 print('Meu nome é: ' + nome)
-print('Eu tenho: ' + idade + 'anos.')
+print('Eu tenho: ' + idade + ' anos.')
